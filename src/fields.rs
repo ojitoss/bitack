af36343ce4d267535bc25aa68eeb76ff80d128a6
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-07-19 15:48:47 -0700

    refactor(fields): change test to use tuples insted of loops
    The old test was less scalable when add new options to verify.
    
    - in 'resolver' method, change name of 'some' var to be more clarify

--- src/fields.rs
+++ src/fields.rs
@@ -19,11 +19,11 @@ pub(crate) fn resvoler(bits: Vec<BitField>) -> Vec<Resolvers> {
     for bit in &bits {
         match bit {
             BitField::Next(bit_amount) => {
-                let some = left_bitmask_info::<u32>(*bit_amount as usize);
+                let mask_info = left_bitmask_info::<u32>(*bit_amount as usize);
 
                 masks.push(Resolvers::Base {
-                    shift: (some.shift - (acc as usize)) as u32,
-                    mask: some.mask >> some.shift
+                    shift: (mask_info.shift - (acc as usize)) as u32,
+                    mask: mask_info.mask >> mask_info.shift
                 });
                 
                 acc += bit_amount;
@@ -52,25 +52,20 @@ mod test {
 
         let resolvers = resvoler(layout);
         assert_eq!(3, resolvers.len());
-        for i in 0..3 {
-            let resolver = &resolvers[i];
-            match resolver {
-                Resolvers::Base { shift, mask } => {
-                    if i == 0 {
-                        assert_eq!(&0b11, mask);
-                        assert_eq!(&30, shift);
-                    }
 
-                    if i == 1 {
-                        assert_eq!(&0b00_111111, mask);
-                        assert_eq!(&24, shift);
-                    }
+        let cases: Vec<(&u32, &u32, usize)> = vec![
+            (&0b11, &30, 0),
+            (&0b00_111111, &24, 1),
+            (&0b00000000_0000_1111, &16, 2)
+        ];
 
-                    if i == 2 {
-                        assert_eq!(&0b00000000_0000_1111, mask);
-                        assert_eq!(&16, shift);
-                    }
-                }
+        for (expected_mask, expected_shift, index) in cases {
+            let resolver = &resolvers[index];
+
+            #[allow(irrefutable_let_patterns)]
+            if let Resolvers::Base { shift, mask } = resolver {
+                assert_eq!(expected_shift, shift);
+                assert_eq!(expected_mask, mask);
             }
         }
     }
