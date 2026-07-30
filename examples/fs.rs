0342d594a83627bc8af360ee164f2908658878ca
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-07-30 15:03:35 -0700

    fix: example generate a temporary file to can be pass publish tests

--- examples/fs.rs
+++ examples/fs.rs
@@ -1,8 +1,8 @@
-use std::{fs};
+use std::{fs, env};
 use bitack::{BitScheme, BitField};
 
 fn main() {
-    let path = "bitack";
+    let path = env::temp_dir().join("bitack");
     let scheme = BitScheme::new(vec![
         BitField::Next(2),
         BitField::Next(4),
@@ -16,8 +16,8 @@ fn main() {
 
     let content = scheme.write(expecteds.clone());
 
-    let _ = fs::write(path, content.unwrap(true));
-    let bytes = fs::read(path);
+    let _ = fs::write(&path, content.unwrap(true));
+    let bytes = fs::read(&path);
 
     if let Ok(bytes) = bytes {
         let bytes = scheme.read(bytes);
