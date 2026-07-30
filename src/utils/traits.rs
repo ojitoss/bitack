851a98b3536d0dcb6c8bf7ac50b767a97fa9b389
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-07-29 19:04:50 -0700

    chore(utils): create a macro rule for repetitive implement block in 'BitUInt' trait

--- src/utils/traits.rs
+++ src/utils/traits.rs
@@ -16,42 +16,23 @@ pub trait BitUint:
     fn leading_zeros(&self) -> u32;
 }
 
-impl BitUint for u8  { 
-    const BITS: usize = 8; 
-    const MAX: Self = !0;
+macro_rules! impl_bituint {
+    ($($uint_type:ty),+ $(,)?) => {
+        $(
+            impl BitUint for $uint_type {
+                const BITS: usize = <$uint_type>::BITS as usize;
+                const MAX: Self = <$uint_type>::MAX;
 
-    fn leading_ones(&self) -> u32 { u8::leading_ones(*self) }
-    fn leading_zeros(&self) -> u32 { u8::leading_zeros(*self) }
+                fn leading_ones(&self) -> u32 {
+                    <$uint_type>::leading_ones(*self)
+                }
+
+                fn leading_zeros(&self) -> u32 {
+                    <$uint_type>::leading_zeros(*self)
+                }
+            }
+        )+
+    };
 }
 
-impl BitUint for u16 { 
-    const BITS: usize = 16;
-    const MAX: Self = !0;
-
-    fn leading_ones(&self) -> u32 { u16::leading_ones(*self) }
-    fn leading_zeros(&self) -> u32 { u16::leading_zeros(*self) }
-}
-
-impl BitUint for u32 { 
-    const BITS: usize = 32;
-    const MAX: Self = !0;
-
-    fn leading_ones(&self) -> u32 { u32::leading_ones(*self) }
-    fn leading_zeros(&self) -> u32 { u32::leading_zeros(*self) }
-}
-
-impl BitUint for u64 { 
-    const BITS: usize = 64; 
-    const MAX: Self = !0;
-
-    fn leading_ones(&self) -> u32 { u64::leading_ones(*self) }
-    fn leading_zeros(&self) -> u32 { u64::leading_zeros(*self) }
-}
-
-impl BitUint for u128 {
-    const BITS: usize = 128;
-    const MAX: Self = !0;
-
-    fn leading_ones(&self) -> u32 { u128::leading_ones(*self) }
-    fn leading_zeros(&self) -> u32 { u128::leading_zeros(*self) } 
-}
+impl_bituint!(u8, u16, u32, u64, u128);
