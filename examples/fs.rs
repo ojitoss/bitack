2ae8832a8047aa6b96e1ab7866c5449499d859e3
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-07-30 11:09:57 -0700

    feat: add example of make an read a binary file

---
+++ examples/fs.rs
@@ -0,0 +1,30 @@
+use std::{fs};
+use bitack::{BitScheme, BitField};
+
+fn main() {
+    let path = "bitack";
+    let scheme = BitScheme::new(vec![
+        BitField::Next(2),
+        BitField::Next(4),
+        BitField::Skip(4)
+    ]);
+
+    let expecteds = vec![
+        2,
+        15
+    ];
+
+    let content = scheme.write(expecteds.clone());
+
+    let _ = fs::write(path, content.unwrap(true));
+    let bytes = fs::read(path);
+
+    if let Ok(bytes) = bytes {
+        let bytes = scheme.read(bytes);
+        
+        for i in 0..expecteds.len() {
+            let getted = bytes.get(i);
+            assert_eq!(expecteds[i], getted);
+        }
+    }
+}
