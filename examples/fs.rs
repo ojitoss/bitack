532e91b4428d0b4c425ac040c8282866db72f809
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-06 15:43:52 -0700

    fix: remove on the fs example a uneccessary ref

--- examples/fs.rs
+++ examples/fs.rs
@@ -1,8 +1,8 @@
-use std::{fs, env};
+use std::{fs};
 use bitack::{BitScheme, BitField};
 
 fn main() {
-    let path = env::temp_dir().join("bitack");
+    let path = "bitack";
     let scheme = BitScheme::new(vec![
         BitField::Next(2),
         BitField::Next(4),
@@ -16,8 +16,8 @@ fn main() {
 
     let content = scheme.write(expecteds.clone());
 
-    let _ = fs::write(&path, content.unwrap(true));
-    let bytes = fs::read(&path);
+    let _ = fs::write(path, content.unwrap(true));
+    let bytes = fs::read(path);
 
     if let Ok(bytes) = bytes {
         let bytes = scheme.read(bytes);
